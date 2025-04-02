794e075af3dcd3f9ae04b7160185834cfdbc10f0
Avrum Kline <klineavrum@gmail.com>
2025-04-02 10:27:58 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scnr = new Scanner(System.in);
+
+        while (true) {
+
+        }
+
+        scnr.close();
+
+    }
+}
