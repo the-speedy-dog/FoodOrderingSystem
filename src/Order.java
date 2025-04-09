777e9d6e0fe9161955fc5582d18ae25721bbbbb6
Avrum Kline <klineavrum@gmail.com>
2025-04-09 16:37:28 -0700

    Order class should be complete

--- src/Order.java
+++ src/Order.java
@@ -1,3 +1,58 @@
 public class Order {
-    private int id;
+    private int ID;
+    private String customerName;
+    private String address;
+    private String driverName;
+    private double totalPrice;
+    private ArrayList<FoodItem> items;
+
+    public Order(String customerName, String address) {
+        this.ID = (int)(Math.random() * Integer.MAX_VALUE);
+        this.customerName = customerName;
+        this.address = address;
+        this.driverName = "Not Assigned";
+        this.totalPrice = 0.0;
+        this.items = new ArrayList<>();
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setDriverName(String name) {
+        this.driverName = name;
+    }
+
+    public String getDriverName() {
+        return driverName;
+    }
+
+    public int getNumItems() {
+        return items.size();
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public double getTotalPrice() {
+        return totalprice;
+    }
+
+    public void addItem(FoodItem item) {
+        this.items.add(item);
+    }
+    
+    public FoodItem removeItem(String name) {
+        for (int i = 0; i < items.size(); i++) {
+            if (items.getName() == name) {
+                return items.remove(i);
+            }
+        }
+        return new FoodItem("Nothing", 0.0);
+    }
 }
