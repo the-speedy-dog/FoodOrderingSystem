7ffc9f0f560f489155c10a146bfc57c9d8b583c4
Gavin Holmes <gavin@gav.codes>
2025-04-10 01:29:08 -0700

    restaurant price rank, higher is more expensive

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -1,31 +1,45 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Restaurant {
     private String name;
     private ArrayList<FoodItem> menu;
     private Rating rating;
+    private int priceRank;
 
 
     public Restaurant(
         String name,
         ArrayList<FoodItem> menu,
         double rating,
-        int ratingCount
+        int ratingCount,
+        int priceRank
     ) {
         this.name = name;
         this.menu = menu;
         this.rating = new Rating(rating, ratingCount);
+        this.priceRank = priceRank;
     }
 
     public String getName() {
         return name;
     }
-    
+
+    public int getPriceRank() {
+        return priceRank;
+    }
+
+    public String getPriceRankString() {
+        char[] charArr = new char[priceRank];
+        Arrays.fill(charArr, '$');
+        return new String(charArr);
+    }
     
     public void  printMenu() {
         for (int i = 0; i < menu.size(); i++) {
             System.out.printf(
-                "%d: %s ($%,.2f)\t\t",
+                "%s%d: %s ($%,.2f)\t\t",
+                i < 9 ? " " : "",
                 i+1,
                 menu.get(i).getName(),
                 menu.get(i).getPrice()
@@ -37,8 +51,13 @@ public class Restaurant {
     }
     
     public void printDetails() {
-        System.out.printf("%s - ★ %.1f (%,d)\n", 
-                name, rating.getRating(), rating.getRatingCount());
+        System.out.printf(
+            "%s - ★ %.1f (%,d) - %s\n", 
+            name,
+            rating.getRating(),
+            rating.getRatingCount(),
+            getPriceRank()
+        );
     }
 
 }
