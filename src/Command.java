714026ff69c6b9a3d6d12f83a7ffdfb034e5631d
Gavin Holmes <96849112+gavrh@users.noreply.github.com>
2025-04-14 10:18:02 -0700

    command implementation

---
+++ src/Command.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+public class Command {
+    private ArrayList<String> names;
+    private String description;
+    private Runnable action;
+
+    public Command(
+        ArrayList<String> names,
+        String description,
+        Runnable action
+    ) {
+        this.names = names;
+        this.description = description;
+        this.action = action;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void printDetails() {
+        for (int i = 0; i < names.size(); i++) {
+            System.out.printf(
+                "%s%s",
+                names.get(i),
+                i == names.size()-1 ? "\n" : ", "
+            );
+        }
+        System.out.println("\t" + description);
+    }
+
+    public boolean includes(String name) {
+        for (String n : names) {
+            if (n.equals(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void run() {
+        action.run();
+    }
+}
